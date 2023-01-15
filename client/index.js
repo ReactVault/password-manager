65b84816bd58515eab96383bcbc248e0753e52ea
Pauline Nguyen <paulinekpn@gmail.com>
2023-01-15 00:33:52 -0800

    Authentication - routers and middleware complete for login and signup

--- client/index.js
+++ client/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import App from './components/App.js';
 import { createRoot } from 'react-dom/client';
 
+import LoginPage from './components/LoginPage.js';
+import SignupPage from './components/SignupPage.js';
 
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App /> );
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App /> );
+root.render(<SignupPage /> );
