c8e3800a054be7f5c5c596758f738c4fbcb07589
wang9hu <xwang.9hu@gmail.com>
2023-01-16 09:58:00 -0800

    db test partially done

--- server/server.js
+++ server/server.js
@@ -6,14 +6,15 @@ const PORT = 3000;
     
 
 // statically serve everything in the build folder on the route '/build'
-app.use('/build', express.static(path.join(__dirname, '/build')));
+app.use('/build', express.static(path.join(__dirname, '/dist')));
 // serve index.html on the route '/'
 app.get('/', (req, res) => {
-    return res.status(200).sendFile(path.join(__dirname, '/index.html'));
+    return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
+
   
 
 
 
-app.listen(3000); //listens on port 3000 -> http://localhost:3000/seryt
+module.exports = app.listen(3000); //listens on port 3000 -> http://localhost:3000/seryt
 
